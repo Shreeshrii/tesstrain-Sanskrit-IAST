bebea333515b6a5d4df2e61d3a89148dc9dcb288
Shree Devi Kumar <5095331+Shreeshrii@users.noreply.github.com>
2021-11-12 06:53:28 +0000

    Add groundtruth for test files

--- test/test.sh
+++ test/test.sh
@@ -5,3 +5,9 @@ for f in *.png; do
        time -p tesseract ${f} ${f%.*}-${lang} -l ${lang} --tessdata-dir ../tessdata_fast 
     done
 done
+
+for f in *.png; do 
+    for lang in IAST IASTuned_0.101000_1896935_14312000 IASTuned_0.088000_1951804_15423100; do
+       time -p tesseract ${f} ${f%.*}-${lang} -l ${lang} --tessdata-dir ../tessdata_best 
+    done
+done
